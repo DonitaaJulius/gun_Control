a2d953c6eed40f15b9042d273928941635c46358
EC2 Default User <ec2-user@ip-172-31-10-27.us-east-2.compute.internal>
2018-08-03 17:47:34 +0000

    first trial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,42 @@ class ApplicationController < Sinatra::Base
   get '/' do
     return erb :index
   end
+  
+  post '/' do
+    @stats1_response = params[:stats1]
+    @stats2_response = params[:stats2]
+    @stats3_response = params[:stats3]
+    @stats4_response = params[:stats4]
+    @permit_response = params[:permit]
+    @cost_response = params[:cost]
+    @valid_response = params[:valid]
+    @reduce_response = params[:reduce]
+    @defense_response = params[:defense]
+    @support_response = params[:support]
+    
+    gun_quiz = Gun_Control.new
+    @tally_array = gun_quiz.play_quiz(@stats1_response, @stats2_response, @stats3_response, @stats4_response, @permit_response, 
+      @cost_response, @valid_response, @reduce_response, @defense_response,@support_response)
+    @tally_events = @tally_array[0]
+    @tally_law = @tally_array[1]
+    @tally_stats = @tally_array[2]
+      
+    if (@tally_events <= 10 && @tally_law <= 10) && @tally_stats <= 20
+      return erb :oregon
+    elsif @tally_events <= 10 && @tally_law <= 10
+      return erb :oregon
+    elsif @tally_events <= 10 && @tally_stats <= 20
+      return erb :oregon
+    elsif @tally_stats <= 20 && @tally_law <= 10
+      return erb :oregon
+    elsif @tally_stats <= 20
+      return erb :stats
+    elsif @tally_law <= 10
+      return erb :law
+    elsif @tally_events <= 10 
+      return erb :events
+    elsif @tally_events >= 20 && @tally_law >= 20 && @tally_stats >= 30
+      return erb :protests
+    end 
+  end 
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,104 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+class Gun_Control
+    
+  def initialize
+      @tally_stats = 0
+      @tally_law = 0
+      @tally_events = 0
+      @subjects = {"correct" => ["350 million", "13000", "US", "290", "21", "65", "4", "Doing background checks", "60000-120000","not as much as it should be"]}
+  end 
+  
+  def q1(answer)
+      if answer == @subjects["correct"][0]
+          @tally_stats += 10
+      else 
+          @tally_stats -=0
+      end
+  end 
+  
+  def q2(answer)
+      if answer == @subjects["correct"][1]
+          @tally_stats += 10
+      else 
+          @tally_stats -=0
+      end
+  end 
+  
+  def q3(answer)
+      if answer == @subjects["correct"][2]
+          @tally_stats += 10
+      else 
+          @tally_stats -=0
+      end
+  end 
+  
+  def q4(answer)
+      if answer == @subjects["correct"][3]
+          @tally_stats += 10
+      else 
+          @tally_stats -= 0
+      end
+  end 
+  
+  def q5(answer)
+      if answer == @subjects["correct"][4]
+          @tally_law += 10
+      else 
+          @tally_law -= 0
+      end
+  end 
+  
+  def q6(answer)
+      if answer == @subjects["correct"][5]
+          @tally_law += 10
+      else 
+          @tally_law -= 0
+      end
+  end 
+      
+  def q7(answer)
+      if answer == @subjects["correct"][6]
+          @tally_law += 10
+      else 
+          @tally_law -=0
+      end
+  end 
+      
+  def q8(answer)
+      if answer == @subjects["correct"][7]
+          @tally_events += 10
+      else 
+          @tally_events -=0
+      end
+  end 
+      
+  def q9(answer)
+      if answer == @subjects["correct"][8]
+          @tally_events += 10
+      else 
+          @tally_events -=0
+      end
+  end 
+  
+  def q10(answer)
+      if answer == @subjects["correct"][9]
+          @tally_events += 10
+      else 
+          @tally_events -=0
+      end
+  end 
+      
+  def play_quiz(stats1, stats2, stats3, stats4, permit, cost, valid, reduce, defense, support)
+      q1(stats1)
+      q2(stats2)
+      q3(stats3)
+      q4(stats4)
+      q5(permit)
+      q6(cost)
+      q7(valid)
+      q8(reduce)
+      q9(defense)
+      q10(support)
+      
+      return [@tally_events, @tally_law, @tally_stats]
+  end 
+end 
